8b8700948dd5c8d9a51b67c96eb60b1906d52da6
jackconwayiv <jackconwayiv@gmail.com>
2023-03-05 15:13:04 -0700

    save, load, & reset properties on localStorage

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //initialize data
-let data = {};
+let data = { food: 0, wood: 0, timer: 0 };
 
 function initialize() {
-  if (window.localStorage.getItem("score") > 4000) {
+  if (window.localStorage.getItem("timer") > 0) {
     loadData(data);
   } else {
     reset();
@@ -13,35 +13,52 @@ function initialize() {
 initialize();
 
 function saveData(data) {
-  window.localStorage.setItem("score", data.score);
+  window.localStorage.setItem("food", data.food);
+  window.localStorage.setItem("wood", data.wood);
+  window.localStorage.setItem("timer", data.timer);
 }
 
 function loadData(data) {
-  data.score = window.localStorage.getItem("score");
+  data.food = window.localStorage.getItem("food");
+  data.timer = window.localStorage.getItem("timer");
+  data.wood = window.localStorage.getItem("wood");
   updateScoreboard(data);
 }
 
-function add(data) {
-  data.score++;
+function addFood(data) {
+  data.food++;
+  saveData(data);
+  updateScoreboard(data);
+}
+
+function addWood(data) {
+  data.wood++;
   saveData(data);
   updateScoreboard(data);
 }
 
 function reset() {
   window.localStorage.clear();
-  window.localStorage.setItem("score", 4000);
-  data.score = window.localStorage.getItem("score");
+  window.localStorage.setItem("food", 0);
+  window.localStorage.setItem("timer", 0);
+  window.localStorage.setItem("wood", 0);
+  loadData(data);
   updateScoreboard(data);
 }
 
 function updateScoreboard(data) {
-  document.getElementById("scoreboard").innerText = data.score;
+  document.getElementById("food").innerText = data.food + " food";
+  document.getElementById("wood").innerText = data.wood + " wood";
+  document.getElementById("timer").innerText = data.timer + " timer";
 }
 
 function tick() {
-  data.score++;
+  data.timer++;
+  data.food++;
+  data.wood++;
   saveData(data);
   updateScoreboard(data);
+  // console.log(localStorage);
 }
 
 setInterval(tick, 1000);
