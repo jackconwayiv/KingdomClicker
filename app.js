75f181cfd9040ef29401307d15e01e65ae9950cd
jackconwayiv <jackconwayiv@gmail.com>
2023-03-05 19:11:55 -0700

    basic css, stuff-per-sec, unlock scoreboard

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //initialize data
-const iData = {
+const initialData = {
   timer: 0,
   level: 1,
   gold: 0,
@@ -12,6 +12,11 @@ const iData = {
   stonePer: 0,
   metal: 0,
   metalPer: 0,
+  market: false,
+  quarry: false,
+  mines: false,
+  camp: false,
+  farm: true,
 };
 
 function convertToJson(data) {
@@ -23,7 +28,7 @@ function convertFromJson(data) {
 }
 
 function initialize() {
-  let data = JSON.parse(window.localStorage.getItem("data")) || iData;
+  let data = JSON.parse(window.localStorage.getItem("data")) || initialData;
   if (data.timer > 0) {
     loadData(data);
   } else {
@@ -41,52 +46,62 @@ function saveData() {
 
 function loadData() {
   data = JSON.parse(window.localStorage.getItem("data"));
-  updateScoreboard(data);
+  updateScoreboard();
 }
 
 function addFood() {
   data.food++;
-  saveData(data);
-  updateScoreboard(data);
+  saveData();
+  updateScoreboard();
 }
 
 function addWood() {
   data.wood++;
-  saveData(data);
-  updateScoreboard(data);
+  saveData();
+  updateScoreboard();
 }
 
 function addMetal() {
   data.metal++;
-  saveData(data);
-  updateScoreboard(data);
+  saveData();
+  updateScoreboard();
 }
 
-function addStone(data) {
+function addStone() {
   data.stone++;
-  saveData(data);
-  updateScoreboard(data);
+  saveData();
+  updateScoreboard();
 }
 
 function reset() {
   window.localStorage.clear();
-  let jData = JSON.stringify(iData);
+  let jData = JSON.stringify(initialData);
   window.localStorage.setItem("data", jData);
   loadData();
   updateScoreboard();
 }
 
-function updateScoreboard(data) {
-  document.getElementById("timer").innerText = "🕰️ " + data.timer;
-  document.getElementById("gold").innerText = "💰 " + data.gold;
-  document.getElementById("food").innerText = "🌾 " + data.food;
-  document.getElementById("wood").innerText = "🪵 " + data.wood;
-  document.getElementById("stone").innerText = "🧱 " + data.stone;
-  document.getElementById("metal").innerText = "🔩 " + data.metal;
+//line 86 is throwing an error that doesn't affect functionality
+function updateScoreboard() {
+  document.querySelector(".townHall").innerText =
+    "TOWN HALL\n\n🕰️ " + data.timer + "\n\nRESET GAME";
+  if (data.market)
+    document.getElementById("gold").innerText = "💰 " + data.gold;
+  if (data.farm) document.getElementById("food").innerText = "🌾 " + data.food;
+  if (data.camp) document.getElementById("wood").innerText = "🪵 " + data.wood;
+  if (data.quarry)
+    document.getElementById("stone").innerText = "🧱 " + data.stone;
+  if (data.mines)
+    document.getElementById("metal").innerText = "🔩 " + data.metal;
 }
 
 function tick() {
   data.timer++;
+  data.gold += data.goldPer;
+  data.food += data.foodPer;
+  data.wood += data.woodPer;
+  data.stone += data.stonePer;
+  data.metal += data.metalPer;
   saveData(data);
   updateScoreboard(data);
   // console.log(localStorage);
