5978f5471e119db7d23957ed2c85f7d020fd03bc
jackconwayiv <jackconwayiv@gmail.com>
2023-03-06 00:00:03 -0700

    render buildings functions, more css styling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //initialize data
 const initialData = {
   timer: 0,
-  level: 1,
+  level: 0,
   gold: 0,
   goldPer: 0,
   food: 0,
@@ -12,11 +12,10 @@ const initialData = {
   stonePer: 0,
   metal: 0,
   metalPer: 0,
-  market: false,
-  quarry: false,
-  mines: false,
-  camp: false,
-  farm: true,
+  market: true,
+  quarry: true,
+  mines: true,
+  camp: true,
 };
 
 function convertToJson(data) {
@@ -29,12 +28,55 @@ function convertFromJson(data) {
 
 function initialize() {
   let data = JSON.parse(window.localStorage.getItem("data")) || initialData;
+  renderTownHall();
+  renderFarm();
+  renderCamp();
+  renderQuarry();
+  renderMines();
+  renderMarket();
   if (data.timer > 0) {
     loadData(data);
   } else {
     reset();
   }
+
   updateScoreboard(data);
+}
+
+function renderTownHall() {
+  let townHall = document.getElementById("townHallPlot");
+  const html = `<div id="townHall"><div class="buildingName">TOWN HALL</div><div id="townTalk" class="smallerFont">🏰 Grow the realm!</div><div id="clock"></div><div id="resetButtonField"><button id="resetButton" onclick="reset()">Reset Kingdom</button></div></div>`;
+  townHall.innerHTML = html;
+}
+
+function renderFarm() {
+  let farm = document.getElementById("farmPlot");
+  const html = `<div id="farm"><div class="buildingName">FARM</div><div id="foodPer" class="smallerFont"></div><div id="food"></div><div id="foodButtonField"><button id="foodButton" onclick="addFood()">Add Food</button></div></div>`;
+  farm.innerHTML = html;
+}
+
+function renderCamp() {
+  let camp = document.getElementById("campPlot");
+  const html = `<div id="camp"><div class="buildingName">LUMBER CAMP</div><div id="woodPer" class="smallerFont"></div><div id="wood"></div><div id="woodButtonField"><button id="woodButton" onclick="addWood()">Add Wood</button></div></div>`;
+  camp.innerHTML = html;
+}
+
+function renderQuarry() {
+  let quarry = document.getElementById("quarryPlot");
+  const html = `<div id="quarry"><div class="buildingName">QUARRY</div><div id="stonePer" class="smallerFont"></div><div id="stone"></div><div id="stoneButtonField"><button id="stoneButton" onclick="addStone()">Add Stone</button></div></div>`;
+  quarry.innerHTML = html;
+}
+
+function renderMines() {
+  let mines = document.getElementById("minesPlot");
+  const html = `<div id="mines"><div class="buildingName">MINES</div><div id="metalPer" class="smallerFont"></div><div id="metal"></div><div id="metalButtonField"><button id="metalButton" onclick="addMetal()">Add Metal</button></div></div>`;
+  mines.innerHTML = html;
+}
+
+function renderMarket() {
+  let market = document.getElementById("marketPlot");
+  const html = `<div id="market"><div class="buildingName">MARKET</div><div id="goldPer" class="smallerFont"></div><div id="gold"></div><div id="goldButtonField"><button id="goldButton" onclick="addGold()">Add Gold</button></div></div>`;
+  market.innerHTML = html;
 }
 
 initialize();
@@ -73,6 +115,12 @@ function addStone() {
   updateScoreboard();
 }
 
+function addGold() {
+  data.gold++;
+  saveData();
+  updateScoreboard();
+}
+
 function reset() {
   window.localStorage.clear();
   let jData = JSON.stringify(initialData);
@@ -83,16 +131,30 @@ function reset() {
 
 //line 86 is throwing an error that doesn't affect functionality
 function updateScoreboard() {
-  document.querySelector(".townHall").innerText =
-    "TOWN HALL\n\n🕰️ " + data.timer + "\n\nRESET GAME";
-  if (data.market)
+  document.querySelector("#clock").innerText = "🕰️ " + data.timer;
+  document.getElementById("food").innerText = "🌾 " + data.food;
+  document.getElementById("foodPer").innerText =
+    data.foodPer + " 🌾 per second";
+  if (data.market) {
     document.getElementById("gold").innerText = "💰 " + data.gold;
-  if (data.farm) document.getElementById("food").innerText = "🌾 " + data.food;
-  if (data.camp) document.getElementById("wood").innerText = "🪵 " + data.wood;
-  if (data.quarry)
+    document.getElementById("goldPer").innerText =
+      data.goldPer + " 💰 per second";
+  }
+  if (data.camp) {
+    document.getElementById("wood").innerText = "🪵 " + data.wood;
+    document.getElementById("woodPer").innerText =
+      data.woodPer + " 🪵 per second";
+  }
+  if (data.quarry) {
     document.getElementById("stone").innerText = "🧱 " + data.stone;
-  if (data.mines)
+    document.getElementById("stonePer").innerText =
+      data.stonePer + " 🧱 per second";
+  }
+  if (data.mines) {
     document.getElementById("metal").innerText = "🔩 " + data.metal;
+    document.getElementById("metalPer").innerText =
+      data.metalPer + " 🔩 per second";
+  }
 }
 
 function tick() {
